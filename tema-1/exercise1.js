f4c697a0e813742c20f28d7fbd261d771c5d1f6e
Carlos Larriu <carlos.larriu.intern@veridas.com>
2022-09-14 23:02:09 +0200

    exercises from topic 1

---
+++ tema-1/exercise1.js
@@ -0,0 +1,6 @@
+
+let a = "hello world";
+const PI = 3.14;
+
+console.log(a);
+console.log(PI);
---
+++ tema-1/exercise5.js
@@ -0,0 +1,18 @@
+
+class Car {
+
+    constructor(brand, model, year, km) {
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+        this.km = km;
+    }
+
+    print() {
+        let response = `The car is a ${this.brand} ${this.model}, is from ${this.year} and has ${this.km}Km`
+        console.log(response)
+    }
+}
+
+let my_car = new Car("Fiat", "Panda", 2004, 150000)
+my_car.print()
