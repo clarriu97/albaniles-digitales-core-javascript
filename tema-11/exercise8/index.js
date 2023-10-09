94d456777941715d302d3418b236642e5e880bed
Carlos Larriu <clarriu@veridas.com>
2023-10-09 21:36:05 +0200

    added tema-11 exercise 8

---
+++ tema-11/exercise8/index.js
@@ -0,0 +1,19 @@
+
+let url = `https://jsonplaceholder.typicode.com/posts/`;
+
+let table = document.getElementById("table");
+
+fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        console.log(`Number of posts received: ${data.length}`);
+        data.forEach(post => {
+            console.log(post.title)
+            let row = table.insertRow();
+            let titleCell = row.insertCell();
+            let bodyCell = row.insertCell();
+            titleCell.innerHTML = post.title;
+            bodyCell.innerHTML = post.body;
+        });
+    })
+    .catch(error => console.error(error));
