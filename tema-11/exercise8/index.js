db159b6e671533c1196f740f39064e7e32d4ee6c
Carlos Larriu <clarriu@veridas.com>
2023-10-10 20:48:36 +0200

    added some testing

--- tema-11/exercise8/index.js
+++ tema-11/exercise8/index.js
@@ -1,19 +1,23 @@
 
-let url = `https://jsonplaceholder.typicode.com/posts/`;
+function fetchPosts() {
+  let url = `https://jsonplaceholder.typicode.com/posts/`;
 
-let table = document.getElementById("table");
+  let table = document.getElementById("table");
 
-fetch(url)
-    .then(response => response.json())
-    .then(data => {
-        console.log(`Number of posts received: ${data.length}`);
-        data.forEach(post => {
-            console.log(post.title)
-            let row = table.insertRow();
-            let titleCell = row.insertCell();
-            let bodyCell = row.insertCell();
-            titleCell.innerHTML = post.title;
-            bodyCell.innerHTML = post.body;
-        });
-    })
-    .catch(error => console.error(error));
+  fetch(url)
+      .then(response => response.json())
+      .then(data => {
+          console.log(`Number of posts received: ${data.length}`);
+          data.forEach(post => {
+              console.log(post.title)
+              let row = table.insertRow();
+              let titleCell = row.insertCell();
+              let bodyCell = row.insertCell();
+              titleCell.innerHTML = post.title;
+              bodyCell.innerHTML = post.body;
+          });
+      })
+      .catch(error => console.error(error));
+}
+
+module.exports = { fetchPosts };
