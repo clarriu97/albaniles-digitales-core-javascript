1db7b182b56ee6017bc5906d19ee17f531e428b8
Carlos Larriu <clarriu@veridas.com>
2023-10-16 14:25:17 +0200

    added exercise 1 module 12

---
+++ tema-12/exercise1/exercise1.js
@@ -0,0 +1,14 @@
+
+export function getCommonValues(set1, set2) {
+    let commonValues = new Set();
+    for (let value of set1) {
+        if (set2.has(value)) {
+            commonValues.add(value);
+        }
+    }
+    return commonValues;
+}
+
+console.log(getCommonValues(new Set([1, 2, 3]), new Set([2, 3, 4]))); // Set { 2, 3 }
+console.log(getCommonValues(new Set([1, 2, 3]), new Set([4, 5, 6]))); // Set {}
+console.log(getCommonValues(new Set([1, 2, 3]), new Set([3, 4, 5]))); // Set { 3 }
